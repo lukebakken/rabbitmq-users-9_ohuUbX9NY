6900074d6c94402a1fe936a889eb2a991c51cb5d
Luke Bakken <luke@bakken.io>
2023-11-03 07:07:34 -0700

    Add user-provided files

---
+++ Receive.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory
+{
+	HostName = "<host>",
+	UserName = "<user>",
+	Password = "<password>",
+	VirtualHost = "<virtual_host>"
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var previousDateTime = DateTime.Now;
+var received = 0;
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+	received++;
+
+	var currentTime = DateTime.Now;
+	if (currentTime - previousDateTime > TimeSpan.FromSeconds(1))
+	{
+		Console.WriteLine($"Messages / sec: {received}");
+		previousDateTime = currentTime;
+		received = 0;
+	}
+};
+channel.BasicConsume(queue: "<queue>",
+	autoAck: true,
+	consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ Receive.java
@@ -0,0 +1,48 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.Duration;
+
+public class Receive {
+
+    private final static String QUEUE_NAME = "<queue>";
+	private static int received;
+	private static LocalDateTime previousDateTime;
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("<host>");
+		factory.setUsername("<user>");
+        factory.setPassword("<password>");
+        factory.setVirtualHost("<virtual_host>");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+		received = 0;
+		previousDateTime = LocalDateTime.now();
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+
+			received++;
+
+			LocalDateTime currentTime = LocalDateTime.now();
+
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+			
+			Duration duration = Duration.between(currentTime, previousDateTime);
+			long diff = Math.abs(duration.toSeconds());
+			if (diff > 1)
+			{
+				System.out.println("Messages / sec: " + received);
+				previousDateTime = currentTime;
+				received = 0;
+			}
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+    }
+}
