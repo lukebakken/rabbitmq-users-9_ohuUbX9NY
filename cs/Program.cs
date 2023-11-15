1f4f3261d40f90c6b6b4a9e75a595f79aa73f0fa
Luke Bakken <luke@bakken.io>
2023-11-15 11:44:05 -0800

    Add code to work around the issue

--- cs/Program.cs
+++ cs/Program.cs
@@ -1,4 +1,6 @@
-﻿using RabbitMQ.Client;
+﻿using System.Net;
+using System.Net.Sockets;
+using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
 // ThreadPool.SetMinThreads(16 * Environment.ProcessorCount, 16 * Environment.ProcessorCount);
@@ -11,6 +13,16 @@ var factory = new ConnectionFactory
 	VirtualHost = "/",
 	DispatchConsumersAsync = true,
 };
+
+factory.SocketFactory = (AddressFamily af) =>
+{
+    var socket = new Socket(af, SocketType.Stream, ProtocolType.Tcp)
+    {
+        NoDelay = true,
+    };
+    return new LocalTcpClientAdapter(socket);
+};
+
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
@@ -40,4 +52,92 @@ Task ConsumerOnReceived(object sender, BasicDeliverEventArgs @event)
 	}
 
 	return Task.CompletedTask;
+}
+
+class LocalTcpClientAdapter : ITcpClient
+{
+    private Socket _sock;
+
+    public LocalTcpClientAdapter(Socket socket)
+    {
+        _sock = socket ?? throw new InvalidOperationException("socket must not be null");
+    }
+
+    public virtual Task ConnectAsync(string host, int port)
+    {
+        AssertSocket();
+        return _sock.ConnectAsync(host, port);
+    }
+
+    public virtual Task ConnectAsync(IPAddress ep, int port)
+    {
+        AssertSocket();
+        return _sock.ConnectAsync(ep, port);
+    }
+
+    public virtual void Close()
+    {
+        _sock.Dispose();
+    }
+
+    public virtual void Dispose()
+    {
+        Dispose(true);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            Close();
+        }
+    }
+
+    public virtual NetworkStream GetStream()
+    {
+        AssertSocket();
+        return new NetworkStream(_sock);
+    }
+
+    public virtual Socket Client
+    {
+        get
+        {
+            return _sock;
+        }
+    }
+
+    public virtual bool Connected
+    {
+        get
+        {
+            if (_sock is null)
+            {
+                return false;
+            }
+            return _sock.Connected;
+        }
+    }
+
+    public virtual TimeSpan ReceiveTimeout
+    {
+        get
+        {
+            AssertSocket();
+            return TimeSpan.FromMilliseconds(_sock.ReceiveTimeout);
+        }
+        set
+        {
+            AssertSocket();
+            _sock.ReceiveTimeout = (int)value.TotalMilliseconds;
+        }
+    }
+
+    private void AssertSocket()
+    {
+        if (_sock is null)
+        {
+            throw new InvalidOperationException("Cannot perform operation as socket is null");
+        }
+    }
 }
